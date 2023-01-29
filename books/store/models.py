98699c862d3bad64175ba9dc16d324cf2b355864
Steadypim <ilyagt75@gmail.com>
2023-01-29 18:55:31 +1000

    Like, Bookmarks, Rating

--- books/store/models.py
+++ books/store/models.py
@@ -6,7 +6,8 @@ class Book(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
     author_name = models.CharField(max_length=255, verbose_name='Автор')
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')
 
     def __str__(self):
         return f'id {self.id}: {self.name}'
@@ -29,4 +30,11 @@ class UserBookRelation(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     like = models.BooleanField(default=False)
     in_bookmarks = models.BooleanField(default=False)
-    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.book.name}, RATE {self.rate}'
+
+    class Meta:
+        verbose_name = "Отношение"
+        verbose_name_plural = "Отношения"
