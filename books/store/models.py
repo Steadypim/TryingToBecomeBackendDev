9c7479d99a9df937d2c6bb75d9b2ee5ba1df8a81
Steadypim <ilyagt75@gmail.com>
2023-01-29 17:39:16 +1000

    2 version

--- books/store/models.py
+++ books/store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,6 +6,7 @@ class Book(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
     author_name = models.CharField(max_length=255, verbose_name='Автор')
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'id {self.id}: {self.name}'
@@ -12,3 +14,19 @@ class Book(models.Model):
     class Meta:
         verbose_name = "Книга"
         verbose_name_plural = "Книги"
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Ok'),
+        (2, 'Fine'),
+        (3, 'Good'),
+        (4, 'Amazing'),
+        (5, 'Incredible')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
